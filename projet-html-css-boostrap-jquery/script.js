73af1e7e35d32c4cde86026ca2a97f89427237ea
takfarines-dev <76485376+takfarines-dev@users.noreply.github.com>
2020-12-28 12:34:52 +0100

    Add files via upload

---
+++ projet-html-css-boostrap-jquery/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".navbar").hide();
+
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 140) {
+        $(".navbar").fadeIn();
+      } else {
+        $(".navbar").fadeOut();
+      }
+    });
+  });
+});
